1526e7308811b9188dc26bcb26b22818964d0c87
septicmk <septic.mk@gmail.com>
2016-11-01 14:12:13 +0800

    init

--- bcast_.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FROM="/home/mengke/test/test_rdma/rdma/main"
-TO="/home/mengke/test/test_rdma/rdma/"
-
-scp $FROM  mengke@blade12:$TO
-
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make clean; make
-bash ./bcast_.sh
-mpiexec -n 2 -f ./host ./main
